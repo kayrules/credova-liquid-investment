b926a185d196de9a80fbd7f51d5eabfc93eda631
kayrules <kayrules@gmail.com>
2025-10-21 08:51:47 +0800

    update script

--- publish-website.sh
+++ publish-website.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+sudo ls
 bun run build:website
-sudo mv -rf packages/website/dist /var/www/liquid-website
-sudo chown -R www-data:www-data /var/www/liquid-website
+sudo rm -rf /var/www/liquid-web
+sudo mv -f packages/website/dist /var/www/liquid-web
+sudo chown -R www-data:www-data /var/www/liquid-web
 sudo service nginx restart
